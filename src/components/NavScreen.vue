83bbd292d2ddb4fbe6f6ef68c136478cc139c988
Rebecca <rebj@Rebeccas-MacBook-Pro.local>
2020-06-07 18:30:33 -0400

    added socials to mobile nav screen

--- src/components/NavScreen.vue
+++ src/components/NavScreen.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="nav-screen">
-    <ul>
+    <ul class="main">
       <transition appear name="navlist" v-for="(item, i) in $nav" :key="i">
         <li
           v-if="navOpen"
@@ -8,6 +8,13 @@
           :style="`transition-delay: ${(i * 70) + 100}ms`"
           @click.prevent="handleClick(i)"
         >{{item.title}}</li>
+      </transition>
+    </ul>
+    <ul class="socials link">
+      <transition appear name="navlist" v-for="(item, i) in $navsocials" :key="i">
+        <li v-if="navOpen" :style="`transition-delay: ${(i * 70) + 100}ms`">
+          <a target="_blank" :href="item.link.url">{{$cms.textField(item.label)}}</a>
+        </li>
       </transition>
     </ul>
   </div>
@@ -49,30 +56,51 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 @import "../styles/stylesheet.scss";
-ul {
+.main,
+.socials {
   position: fixed;
   bottom: 2em;
-  left: 3em;
   color: white;
   list-style: none;
   padding: 0;
+}
+.main {
+  left: 3em;
   li {
     font-family: $acumin;
-    margin: 1em 0;
+    margin: 30px 0;
     @include sub();
     opacity: 1;
     transform: translateY(0%);
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+}
+
+.socials {
+  right: 3em;
+  text-align: right;
+  li {
+    font-family: $acuminc;
+    margin: 30px 0;
+    @include body();
+    opacity: 1;
+    transform: translateY(0%);
+    &:last-child {
+      margin-bottom: 0;
+    }
   }
 }
 .navlist-enter,
 .navlist-leave {
-  transform: translateY(100%);
-  opacity: 0;
+  transform: translateY(100%) !important;
+  opacity: 0 !important;
 }
 
 .navlist-enter-to {
-  transform: translateY(0%);
-  opacity: 1;
+  transform: translateY(0%) !important;
+  opacity: 1 !important;
 }
 
 .navlist-enter-active,
@@ -80,7 +108,7 @@ ul {
   @include ease(all);
 }
 .navlist-leave-to {
-  transform: translateY(-100%);
-  opacity: 0;
+  transform: translateY(-100%) !important;
+  opacity: 0 !important;
 }
 </style>
